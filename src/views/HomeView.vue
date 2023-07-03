ccabc675724647f0e763dd521ed7c356566e2b07
Stoksnov <61091479+Stoksnov@users.noreply.github.com>
2023-07-03 07:22:57 +0400

    adding dist

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container">
-    <TickerCardList></TickerCardList>
-  </div>
-</template>
-
-<script setup lang="ts">
-import TickerCardList from '@/components/TickerCardList.vue';
-</script>
